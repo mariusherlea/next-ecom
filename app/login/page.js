2500b45a8adfd928fda770292368d872c0f9d0f7
Herlea Marius <mariusherlea@gmail.com>
2024-07-09 09:52:17 +0300

    session provider as a NextAuth.js option.

---
+++ app/login/page.js
@@ -0,0 +1,78 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import toast from "react-hot-toast";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result?.error) {
+        toast.error(result?.error);
+        setLoading(false);
+      } else {
+        toast.success("Logged in successfully");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  return (
+    <main>
+      <div className="container">
+        <div className="row d-flex justify-content-center align-items-center vh-100">
+          <div className="col-lg-5 shadow bg-light p-5">
+            <h2 className="mb-4 text-center">Login</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control mb-4"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control mb-4"
+                  id="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-raised"
+                  disabled={loading || !email || !password}
+                >
+                  {loading ? "Please wait..." : "Register"}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- components/TopNav.js
+++ components/TopNav.js
@@ -1,6 +1,10 @@
+"use clients";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 export default function TopNav() {
+  const { data, status, loading } = useSession();
+  console.log(data, status, loading);
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <Link href="/" className="nav-link">
