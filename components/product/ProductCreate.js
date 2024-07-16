39ff85d89fc99b2f9d509cbf1f0f0e8059010b5d
Herlea Marius <mariusherlea@gmail.com>
2024-07-16 12:12:02 +0300

    add product input field

--- components/product/ProductCreate.js
+++ components/product/ProductCreate.js
@@ -30,10 +30,93 @@ export default function ProductCreate() {
 
   return (
     <div className="container">
-      Productcreate
+      <p className="lead">{updatingProduct ? "Update" : "Create"} Product</p>
+      <input
+        type="text"
+        placeholder="Title"
+        value={updatingProduct ? updatingProduct?.title : product?.title}
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({ ...updatingProduct, title: e.target.value })
+            : setProduct({ ...product, title: e.target.value })
+        }
+        className="form-control p-2 my-2"
+      />
+
+      <textarea
+        rows="5"
+        className="form-control p-2 mb-2"
+        placeholder="Description"
+        value={
+          updatingProduct ? updatingProduct?.description : product?.description
+        }
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({
+                ...updatingProduct,
+                description: e.target.value,
+              })
+            : setProduct({ ...product, description: e.target.value })
+        }
+      ></textarea>
+      <input
+        type="number"
+        placeholder="Price"
+        min="1"
+        className="form-control p-2 mb-2"
+        value={updatingProduct ? updatingProduct?.price : product?.price}
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({
+                ...updatingProduct,
+                price: e.target.value,
+              })
+            : setProduct({ ...product, price: e.target.value })
+        }
+      />
+
+      <input
+        type="text"
+        placeholder="Color"
+        value={updatingProduct ? updatingProduct?.color : product?.color}
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({ ...updatingProduct, color: e.target.value })
+            : setProduct({ ...product, color: e.target.value })
+        }
+        className="form-control p-2 my-2"
+      />
+
+      <input
+        type="text"
+        placeholder="Brand"
+        value={updatingProduct ? updatingProduct?.brand : product?.brand}
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({ ...updatingProduct, brand: e.target.value })
+            : setProduct({ ...product, brand: e.target.value })
+        }
+        className="form-control p-2 my-2"
+      />
+
+      <input
+        type="number"
+        placeholder="Stock"
+        min="1"
+        className="form-control p-2 mb-2"
+        value={updatingProduct ? updatingProduct?.stock : product?.stock}
+        onChange={(e) =>
+          updatingProduct
+            ? setUpdatingProduct({
+                ...updatingProduct,
+                stock: e.target.value,
+              })
+            : setProduct({ ...product, stock: e.target.value })
+        }
+      />
       <pre>
-        {JSON.stringify(categories, null, 4)}
-        {JSON.stringify(tags, null, 4)}
+        {/* {JSON.stringify(product, null, 4)} */}
+        {/* {JSON.stringify(tags, null, 4)} */}
       </pre>
     </div>
   );
